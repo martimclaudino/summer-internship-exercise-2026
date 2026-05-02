ec7704a313432d622c4f2d80e083709fe7782f4a
Martim Claudino <martimffclaudino@tecnico.ulisboa.pt>
2026-05-02 23:13:51 +0100

    logica implementada

--- src/main/java/com/premiumminds/internship/taskscheduler/TaskSchedulerService.java
+++ src/main/java/com/premiumminds/internship/taskscheduler/TaskSchedulerService.java
@@ -1,17 +1,90 @@
 package com.premiumminds.internship.taskscheduler;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TaskSchedulerService implements ITaskSchedulerService {
 
     @Override
     public List<Task> getEligibleTasks(Collection<Task> tasks) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        List<Task> eligibleTasks = new ArrayList<>();
+        
+        // Mapping tasks for their id
+        Map<String, Task> taskMap = new HashMap<>();
+        for (Task t : tasks) {
+            taskMap.put(t.getId(), t);
+        }
+
+        for (Task task : tasks) {
+            
+            boolean completedDependencies = true;
+            
+            // Check status
+            if (task.getStatus() == TaskStatus.PENDING) {
+                
+                // Check dependencies
+                for (String depId : task.getDependencies()) {
+                    
+                    Task dependentTask = taskMap.get(depId);
+
+                    // Dependencies are checked in getExecutionOrder() so it won't be null
+                    // but hipothethical further implementations could have functions that don't check it
+                    if (dependentTask == null || dependentTask.getStatus() != TaskStatus.COMPLETED) {
+                        completedDependencies = false;
+                        break;
+                    }
+                }  
+                
+                if (completedDependencies) {
+                    eligibleTasks.add(task);
+                } 
+            }
+        }
+        eligibleTasks.sort(Comparator.comparingInt(Task::getPriority));
+
+        return eligibleTasks;
     }
 
     @Override
     public List<Task> getExecutionOrder(Collection<Task> tasks) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<Task> fullOrder = new ArrayList<>();
+        
+        // Mapping tasks for their id, only to check dependencies with less complexity
+        Map<String, Task> taskMap = new HashMap<>();
+        for (Task t : tasks) {
+            taskMap.put(t.getId(), t);
+        }
+
+        // Check for missing dependencies
+        for (Task task : tasks) {
+            for (String depId : task.getDependencies()) {
+                if (!taskMap.containsKey(depId)) {
+                    throw new IllegalArgumentException("Missing dependency: " + depId);
+                }
+            }
+        }
+
+        while (fullOrder.size() != tasks.size()) {
+            
+            // Get eligible tasks
+            List<Task> tasksToExecute = getEligibleTasks(tasks);
+
+            if (tasksToExecute.isEmpty()) {
+                throw new IllegalArgumentException("Circular dependency detected");
+            }
+
+            // Check the eligible tasks
+            for (Task task : tasksToExecute) {
+                task.setStatus(TaskStatus.COMPLETED);
+                fullOrder.add(task);
+            }
+        }
+
+        return fullOrder;
     }
 }
